47a221d5a9fb228d4337ace60cfe47e209c40578
William Scott <wsc0tt@outlook.com>
2025-06-14 13:33:16 -0700

    updated gitignore

--- app/home.jsx
+++ app/home.jsx
@@ -1,7 +1,5 @@
-import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 import '../global.css'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import SimpleButton from '../components/SimpleButton'
 import { router } from 'expo-router'
 
